4b15c7395f62f351aa9e44a445e4b6fad76e3a94
marcostruji <marcostruji@gmail.com>
2011-05-30 17:15:26 +0000

    Widget Simple funciona :D

---
+++ Gesture Call Pro/src/ac/gestureCallPro/ui/widget/WidgetSimpleConfig.java
@@ -0,0 +1,41 @@
+package ac.gestureCallPro.ui.widget;
+
+import ac.gestureCallPro.R;
+import android.app.Activity;
+import android.appwidget.AppWidgetManager;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class WidgetSimpleConfig extends Activity {
+	 
+    private int widgetId = 0;
+ 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+ 
+        //Obtenemos el Intent que ha lanzado esta ventana
+        //y recuperamos sus parámetros
+        Intent intentOrigen = getIntent();
+        Bundle params = intentOrigen.getExtras();
+ 
+        //Obtenemos el ID del widget que se está configurando
+        widgetId = params.getInt(
+                AppWidgetManager.EXTRA_APPWIDGET_ID,
+                AppWidgetManager.INVALID_APPWIDGET_ID);
+ 
+        //Actualizamos el widget tras la configuración
+        AppWidgetManager appWidgetManager =
+            AppWidgetManager.getInstance(WidgetSimpleConfig.this);
+        WidgetSimple.actualizarWidget(WidgetSimpleConfig.this, appWidgetManager, widgetId);
+
+        
+      //Devolvemos como resultado: ACEPTAR (RESULT_OK)
+        Intent resultado = new Intent();
+        resultado.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId);
+        setResult(RESULT_OK, resultado);
+        finish();
+ 
+
+    }
+}
