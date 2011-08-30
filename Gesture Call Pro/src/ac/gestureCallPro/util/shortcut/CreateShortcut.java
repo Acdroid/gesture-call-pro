23dd7fef41f5662adf110a0aac2f8bac97203030
marcostruji <marcostruji@gmail.com>
2011-08-30 08:13:14 +0000

    Create Shorcut añadido que por algún problema no lo estaba

---
+++ Gesture Call Pro/src/ac/gestureCallPro/util/shortcut/CreateShortcut.java
@@ -0,0 +1,27 @@
+package ac.gestureCallPro.util.shortcut;
+
+import ac.gestureCallPro.R;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Parcelable;
+
+public class CreateShortcut {
+
+
+	public static final void create (Context context){
+
+		Intent shortcutIntent = new Intent(Intent.ACTION_MAIN); 
+		shortcutIntent.setClassName(context, context.getApplicationContext().getPackageName() + ".ui.main");
+
+
+		Intent i = new Intent();
+		i.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
+		i.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
+		i.putExtra(Intent.EXTRA_SHORTCUT_NAME, context.getResources().getString(R.string.app_name));
+		Parcelable iconResource = Intent.ShortcutIconResource.fromContext(
+				context,R.drawable.icon); 
+		i.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE,iconResource);
+
+		context.sendBroadcast(i);
+	}
+}
